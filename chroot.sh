be4386e2a3e3b76c2a4e7464c8f983da923a2da5
creio <mailcreio@gmail.com>
2021-02-24 11:21:30 +0300

    24.02.2021 Update

--- chroot.sh
+++ chroot.sh
@@ -14,29 +14,29 @@ pacman -Syy --noconfirm
 sed -i '/User/s/^#\+//' /etc/sddm.conf
 
 lsb_release_sed() {
-    sed -i /etc/lsb-release \
-        -e 's,DISTRIB_ID=.*,DISTRIB_ID=Ctlos,' \
-        -e 's,DISTRIB_RELEASE=.*,DISTRIB_RELEASE=Rolling,' \
-        -e 's,DISTRIB_CODENAME=.*,DISTRIB_CODENAME=Anon,' \
-        -e 's,DISTRIB_DESCRIPTION=.*,DISTRIB_DESCRIPTION=\"Ctlos Linux\",'
+  sed -i /etc/lsb-release \
+    -e 's,DISTRIB_ID=.*,DISTRIB_ID=Ctlos,' \
+    -e 's,DISTRIB_RELEASE=.*,DISTRIB_RELEASE=Rolling,' \
+    -e 's,DISTRIB_CODENAME=.*,DISTRIB_CODENAME=Anon,' \
+    -e 's,DISTRIB_DESCRIPTION=.*,DISTRIB_DESCRIPTION=\"Ctlos Linux\",'
 }
 
 os_release_sed() {
-    sed -i /usr/lib/os-release \
-        -e 's,NAME=.*,NAME=\"Ctlos Linux\",' \
-        -e 's,PRETTY_NAME=.*,PRETTY_NAME=\"Ctlos Linux\",' \
-        -e 's,ID=.*,ID=ctlos,' \
-        -e 's,ID_LIKE=.*,ID_LIKE=arch,' \
-        -e 's,BUILD_ID=.*,BUILD_ID=rolling,' \
-        -e 's,HOME_URL=.*,HOME_URL=\"https://ctlos.github.io\",' \
-        -e 's,DOCUMENTATION_URL=.*,DOCUMENTATION_URL=\"https://ctlos.github.io/wiki\",' \
-        -e 's,SUPPORT_URL=.*,SUPPORT_URL=\"https://forum.ctlos.ru\",' \
-        -e 's,BUG_REPORT_URL=.*,BUG_REPORT_URL=\"https://github.com/ctlos/ctlosiso/issues\",' \
-        -e 's,LOGO=.*,LOGO=ctlos,'
+  sed -i /usr/lib/os-release \
+    -e 's,NAME=.*,NAME=\"Ctlos Linux\",' \
+    -e 's,PRETTY_NAME=.*,PRETTY_NAME=\"Ctlos Linux\",' \
+    -e 's,ID=.*,ID=ctlos,' \
+    -e 's,ID_LIKE=.*,ID_LIKE=arch,' \
+    -e 's,BUILD_ID=.*,BUILD_ID=rolling,' \
+    -e 's,HOME_URL=.*,HOME_URL=\"https://ctlos.github.io\",' \
+    -e 's,DOCUMENTATION_URL=.*,DOCUMENTATION_URL=\"https://ctlos.github.io/wiki\",' \
+    -e 's,SUPPORT_URL=.*,SUPPORT_URL=\"https://forum.ctlos.ru\",' \
+    -e 's,BUG_REPORT_URL=.*,BUG_REPORT_URL=\"https://github.com/ctlos/ctlosiso/issues\",' \
+    -e 's,LOGO=.*,LOGO=ctlos,'
 }
 
 issue_sed() {
-    sed -i 's|Arch|Ctlos|g' /etc/issue /usr/share/factory/etc/issue
+  sed -i 's|Arch|Ctlos|g' /etc/issue /usr/share/factory/etc/issue
 }
 lsb_release_sed
 os_release_sed
@@ -52,7 +52,7 @@ issue_sed
 LOL
 
 chrooter() {
-   arch-chroot ${work_dir} /bin/bash -c "${1}"
+  arch-chroot ${work_dir} /bin/bash -c "${1}"
 }
 
 chmod +x ${work_dir}/settings.sh
