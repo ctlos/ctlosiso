aa71bd41d07476069f99067c29705969440a7645
creio <mailcreio@gmail.com>
2025-02-14 00:27:42 +0300

    14.02.2025 Update

--- rel.sh
+++ rel.sh
@@ -7,15 +7,15 @@
 
 TAG_NAME=v2.4.7
 
-git tag ${TAG_NAME}
+### release sf
+REPO_SF=ctlos@web.sourceforge.net:/home/pfs/project/ctlos
+SCRIPT_PATH=$(realpath -- ${0%/*})
+ISO_PATH=$SCRIPT_PATH/out
+
+git tag ${TAG_NAME} -F ${ISO_PATH}/README.md
 git push --tags
 
-### release sf
-# REPO_SF=ctlos@web.sourceforge.net:/home/pfs/project/ctlos
-# SCRIPT_PATH=$(realpath -- ${0%/*})
-# ISO_PATH=$SCRIPT_PATH/out
+cp -r $SCRIPT_PATH/rel.md ${ISO_PATH}/README.md
+sed -i "s/TAG_NAME/$TAG_NAME/" ${ISO_PATH}/README.md
 
-# cp -r $SCRIPT_PATH/rel.md ${ISO_PATH}/README.md
-# sed -i "s/TAG_NAME/$TAG_NAME/" ${ISO_PATH}/README.md
-
-# rsync -cauvCLP --delete-excluded --delete "${ISO_PATH}/" "${REPO_SF}/${TAG_NAME}/"
+rsync -cauvCLP --delete-excluded --delete "${ISO_PATH}/" "${REPO_SF}/${TAG_NAME}/"
